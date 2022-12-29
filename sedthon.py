7a013288876201f398a1219cef1329df5beafcde
x49s <119348863+x49s@users.noreply.github.com>
2022-12-28 21:45:46 -0800

    Update sedthon.py

--- sedthon.py
+++ sedthon.py
@@ -570,12 +570,12 @@ async def _(event):
     end = datetime.datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(f'''
-**☆ تـوب سـورس
-☆ الاصدار : 1.2
-☆ البنك : `{ms}`
-☆ التاريخ : `{m9zpi}`
-☆ الايدي : `{event.sender_id}`
-☆ تـوب سـورس : @N1111V**
+**☆ SEDTHON SOURCE
+~ الاصدار : 1.4
+~ البنك : `{ms}`
+~ التاريخ : `{m9zpi}`
+~ الايدي : `{event.sender_id}`
+~ قناة السورس : @SOON**
 ''')
 
 
@@ -669,7 +669,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @IIIT5
+      - @OO2NO
 ''', reply_to=event)
 
 
@@ -678,7 +678,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @IIIT5
+      - @OO2NO
 ''', reply_to=event)
 
 
@@ -687,7 +687,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @IIIT5
+      - @OO2NO
 ''', reply_to=event)
 
 
@@ -696,7 +696,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @IIITT
+      - @OO2NO
 ''', reply_to=event)
 
 
@@ -784,12 +784,12 @@ async def _(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 17])
 
-ownerhson_id = 5244755240
+ownerhson_id = 1964862074
 @sedthon.on(events.NewMessage(outgoing=False, pattern='/start'))
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerhson_id :
-        order = await event.reply('اهلا مطوري حسين - @N1111V')
+        order = await event.reply('Welcom  - @OO2NO')
 
 
 @sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.ورود"))
