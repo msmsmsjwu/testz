e7a24f633e63141c78a427d2636e204524ef204e
x49s <119348863+x49s@users.noreply.github.com>
2023-01-27 22:44:01 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -199,6 +199,19 @@ def gen_user(choice):
             random.shuffle(f)
             username = ''.join(f)
         else:
+            if choice == "11":
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(e)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = ''.join(f)
+        if username in banned[0]:
+            c = random.choices(a)
+            d = random.choices(e)
+            s = random.choices(e)
+            f = [c[0], "_", d[0], "_", s[0]]
+            username = ''.join(f)
+        else:
             pass
     return username
 
