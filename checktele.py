0281a146b541997b5f4c3deecb87469c82c8feb1
x49s <119348863+x49s@users.noreply.github.com>
2023-02-11 00:09:13 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -365,7 +365,7 @@ async def _(event):
     الخطأ :
     {str(eee)}''')
 Threads=[] 
-for t in range(100):
+for t in range(20:
     x = threading.Thread(target=_)
     le = threading.Thread(target=gen_user)
     x.start()
